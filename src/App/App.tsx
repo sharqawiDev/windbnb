193b77ea20e6b5b9c79a0c6209a9559ca5551c59
sharqawidev <sharqawidev@gmail.com>
2022-07-06 18:15:49 +0300

    preparing project assets

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import "./App.scss";
+import logo from "../assets/images/logo.svg";
 
 function App() {
     return (
-        <div className="container">
-            <header className="app-header"></header>
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+            </header>
         </div>
     );
 }
