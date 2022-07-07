6304d9eff6dd4cde545b641356ed56007259245b
sharqawidev <sharqawidev@gmail.com>
2022-07-07 16:00:03 +0300

    fetching data from json file

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,55 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.scss";
 import Header from "./components/Header/Header";
 import StayCard from "./components/StayCard/StayCard";
 import { Card } from "./types";
 
 function App() {
-    const card: Card = {
-        city: "Helsinki",
-        country: "Finland",
-        superHost: true,
-        title: "Stylist apartment in center of the city",
-        rating: 4.4,
-        maxGuests: 3,
-        type: "Entire apartment",
-        beds: 2,
-        photo: "https://images.unsplash.com/photo-1505873242700-f289a29e1e0f?ixlib=rb-1.2.1&auto=format&fit=crop&w=2255&q=80",
-    };
-    const card2: Card = {
-        city: "Helsinki",
-        country: "Finland",
-        superHost: false,
-        title: "Stylist apartment in center of the city",
-        rating: 4.4,
-        maxGuests: 3,
-        type: "Entire apartment",
-        beds: null,
-        photo: "https://images.unsplash.com/photo-1505873242700-f289a29e1e0f?ixlib=rb-1.2.1&auto=format&fit=crop&w=2255&q=80",
-    };
-    const card3: Card = {
-        city: "Helsinki",
-        country: "Finland",
-        superHost: true,
-        title: "Stylist apartment in center of the city",
-        rating: 4.4,
-        maxGuests: 3,
-        type: "Entire apartment",
-        beds: 2,
-        photo: "https://images.unsplash.com/photo-1505873242700-f289a29e1e0f?ixlib=rb-1.2.1&auto=format&fit=crop&w=2255&q=80",
-    };
+    const [cards, setCards] = useState([]);
 
+    useEffect(() => {
+        getStaysData();
+    }, []);
+
+    const getStaysData = async () => {
+        const json = require("../assets/data/stays.json");
+        setCards(json);
+    };
     return (
         <div className="container">
             <Header />
             <div className="stays-title">
                 <h3>Stays in Finland</h3>
-                <p>+12 stays</p>
+                <p>+{cards.length} stays</p>
             </div>
             <div className="cards-container">
-                <StayCard card={card} />
-                <StayCard card={card2} />
-                <StayCard card={card3} />
+                {cards.map((card: Card) => (
+                    <StayCard card={card} key={card.title} />
+                ))}
             </div>
         </div>
     );
